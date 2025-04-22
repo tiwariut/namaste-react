bf2585809c6970b9abeedbc2678ddbfb1d854060
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2025-04-22 19:35:07 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child-1' }, [
     React.createElement('h1', {}, 'Hello from React!'),
