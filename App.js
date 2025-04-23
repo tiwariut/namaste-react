f10eca9392fa3a47ea5be4183068401e8984556d
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2025-04-23 18:54:46 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child-1' }, [
-    React.createElement('h1', {}, 'Hello from React!'),
-    React.createElement('h2', {}, 'Hello from React!')
-  ]),
-  React.createElement('div', { id: 'child-2' }, [
-    React.createElement('h1', {}, 'Hello from React!'),
-    React.createElement('h2', {}, 'Hello from React!')
-  ])
-]);
+const HeaderComponent = () => {
+  return <h1 id='heading'>Namaste React</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(<HeaderComponent />);
